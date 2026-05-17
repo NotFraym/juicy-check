9e9f41ac387d4fa7dcfbdb07ca3af6fcdd933816
notfraym <galois.mathieu02@gmail.com>
2026-05-18 08:40:26 +1100

    fix: update auth config

--- api.juicy-check.nc-main/src/lib/auth/auth.ts
+++ api.juicy-check.nc-main/src/lib/auth/auth.ts
@@ -1,16 +1,25 @@
+import 'dotenv/config';
+
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/drizzle";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, { provider: "pg" }),
+    secret: process.env.BETTER_AUTH_SECRET,
+    trustedOrigins: [process.env.FRONTEND_URL ?? '*'],
+    database: drizzleAdapter(db, {
+        provider: "pg",
+    }),
     emailAndPassword: {
         enabled: true,
         requireEmailVerification: false,
         minPasswordLength: 8,
         maxPasswordLength: 128,
+        autoSignIn: false,
     },
     advanced: {
-        database: { generateId: "uuid" }
+        database: {
+            generateId: "uuid",
+        }
     },
 });
