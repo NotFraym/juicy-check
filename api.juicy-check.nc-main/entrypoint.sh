6ce494ba24c6537ddb3c675370bf799f49d7cce8
notfraym <galois.mathieu02@gmail.com>
2026-05-18 12:21:43 +1100

    allez stp

--- api.juicy-check.nc-main/entrypoint.sh
+++ api.juicy-check.nc-main/entrypoint.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 set -e
 
-echo "Running drizzle-kit push..."
-	npx drizzle-kit push --force
+echo "⏳ Waiting for database..."
 
-echo "Starting application..."
+# attendre que Postgres soit ready
+until nc -z db 5432; do
+  echo "Database not ready yet..."
+  sleep 2
+done
+
+echo "✅ Database is up"
+
+echo "🚀 Running migrations (drizzle-kit push)..."
+npx drizzle-kit push
+
+echo "✅ Migrations done"
+
+echo "▶️ Starting application..."
 exec node dist/main
+
+
+
+
+
+
+
